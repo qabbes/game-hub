fe3910fecc8a43f28c5bec5fffebf5a9e7dcf9e5
qabbes <quentin.abbes@gmail.com>
2024-05-23 00:53:00 +0200

    added Grid template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
   return (
-    <Button colorScheme="teal" size="sm">
-      Button
-    </Button>
+    <Grid
+      templateAreas={'"nav nav" "aside main"'}
+      gridTemplateRows={"50px 1fr 30px"}
+      gridTemplateColumns={"150px 1fr"}
+      h="200px"
+      gap="1"
+      color="blackAlpha.700"
+      fontWeight="bold"
+    >
+      <GridItem pl="2" bg="orange.300" area={"nav"}>
+        NavBar
+      </GridItem>
+      <GridItem pl="2" bg="pink.300" area={"aside"}>
+        Aside
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
