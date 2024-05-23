df0407dfd4d0330ad95b354978ff4a529bac2831
qabbes <quentin.abbes@gmail.com>
2024-05-23 12:36:04 +0200

    added responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
-import { Button, ButtonGroup, Grid, GridItem } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
   return (
     <Grid
-      templateAreas={'"nav nav" "aside main"'}
+      templateAreas={{
+        base: '"nav" "main"',
+        sm: '"nav nav" "aside main"',
+        md: '"nav nav" "aside main"',
+        lg: '"nav nav" "aside main"', //1024px
+      }}
       gridTemplateRows={"50px 1fr 30px"}
-      gridTemplateColumns={"150px 1fr"}
-      h="200px"
+      h="300px"
       gap="1"
       color="blackAlpha.700"
       fontWeight="bold"
@@ -16,9 +20,11 @@ function App() {
       <GridItem pl="2" bg="orange.300" area={"nav"}>
         NavBar
       </GridItem>
-      <GridItem pl="2" bg="pink.300" area={"aside"}>
-        Aside
-      </GridItem>
+      <Show above="sm">
+        <GridItem pl="2" bg="pink.300" area={"aside"}>
+          Aside
+        </GridItem>
+      </Show>
       <GridItem pl="2" bg="green.300" area={"main"}>
         Main
       </GridItem>
