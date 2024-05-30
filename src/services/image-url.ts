dde0f15a0fef8abd2dcef82a3050b5a6bbe6738a
qabbes <quentin.abbes@gmail.com>
2024-05-30 14:16:31 +0200

    handle no image cases

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (url === null || url.length === 0) return "";
+  if (url === null || url.length === 0) return noImage;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   const cropSize = "crop/600/400/";
